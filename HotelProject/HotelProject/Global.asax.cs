6f8ca92a9b293debe6b1f94096b204af6e216a38
https://github.com/HotelPortal/hotel <rennan_paloschi@yahoo.com>
2013-03-24 13:33:18 -0300

    Auto Create
    Coloquei essas linhas pra criar o banco automaticamente já que vcs nao
    tem o banco,
    mas msm assim tem q alterar o arquivo  Web.config e colocar o nome do
    servidor de vcs.

--- HotelProject/HotelProject/Global.asax.cs
+++ HotelProject/HotelProject/Global.asax.cs
@@ -6,6 +6,7 @@ using System.Web.Http;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using HotelProject.Models;
 
 namespace HotelProject
 {
@@ -16,6 +17,10 @@ namespace HotelProject
     {
         protected void Application_Start()
         {
+            var con = new HotelDBContext();
+
+            con.Database.CreateIfNotExists();
+
             AreaRegistration.RegisterAllAreas();
 
             WebApiConfig.Register(GlobalConfiguration.Configuration);
